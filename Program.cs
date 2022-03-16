22c7249d186c2f80629fbc832fa902e2c6452ce4
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2022-03-16 16:28:19 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public abstract class A
+{
+    public abstract void m2();
+    public void m3()
+    {
+        Console.WriteLine(" I am M3 method in abstract");
+    }
+
+}
+
+public class B : A
+{
+    public override void m2()
+    {
+        Console.WriteLine("Hello I am M2 Method");
+    }
+}
+ class AbstractExample
+{
+        public static void Main(string[] args)
+        {
+            A b = new B(); 
+            b.m2();
+            b.m3();
+            Console.ReadLine();
+
+        }
+}
+
